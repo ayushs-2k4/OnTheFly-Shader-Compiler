de5c89285c1464fbda0347d9c7b60290d2094f93
Ayush Singhal <ayushsinghals02@gmail.com>
2024-06-09 11:17:55 +0530

    Initial Commit

---
+++ OnTheFly Shader Compiler/OnTheFly_Shader_CompilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheFly_Shader_CompilerApp.swift
+//  OnTheFly Shader Compiler
+//
+//  Created by Ayush Singhal on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct OnTheFly_Shader_CompilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
